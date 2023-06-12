464f842d13646fea108f15c92e36c5d1723e01c4
diegobajetti <diego.bajetti@outlook.com>
2023-06-11 22:39:49 -0400

    Configure confirm button for Thanh and Nicole

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
-  var button = document.getElementById("confirmationButtonMelvin");
-  button.addEventListener("click", function () {
+  var button1 = document.getElementById("confirmationButtonMelvin");
+  button1.addEventListener("click", function () {
+    $("#confirmation").modal();
+  });
+  var button2 = document.getElementById("confirmationButtonThanh");
+  button2.addEventListener("click", function () {
+    $("#confirmation").modal();
+  });
+  var button3 = document.getElementById("confirmationButtonNicole");
+  button3.addEventListener("click", function () {
     $("#confirmation").modal();
   });
 });

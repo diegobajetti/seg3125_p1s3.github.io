eb717221ffd22be16ea659c60bb0d1b27d3380e4
diegobajetti <diego.bajetti@outlook.com>
2023-06-11 21:28:08 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  var button = document.getElementById("confirmationButtonMelvin");
+  button.addEventListener("click", function () {
+    $("#confirmation").modal();
+  });
+});
